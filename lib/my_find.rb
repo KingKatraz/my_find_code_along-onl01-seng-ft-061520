45a18b11eb5b94af958ad28c2b0d31efc96326e5
Hal H Nader <halnader89@gmail.com>
2020-06-28 15:00:39 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,8 +4,9 @@ def my_find(collection)
   i = 0
   block_return_values = []
   while i < collection.length
-    block_return_values << yield(collection[i])
-    i = i + 1
+    
+    block_return_values << yield(collection[1])
+    i += 1
   end
   
   if block_return_values.include?(false)
